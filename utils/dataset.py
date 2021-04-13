7595faf025513693b49442aaf6319d4e308481eb
BatFaceWayne <44198563+BatFaceWayne@users.noreply.github.com>
2021-04-13 13:57:28 +0200

    Add files via upload

--- utils/dataset.py
+++ utils/dataset.py
@@ -65,19 +65,19 @@ class BasicDataset2(Dataset):
         img_file = glob(self.imgs_dir + idx + '.*')
         #cv2.imread(path,0)
 
-        assert len(depth_file) == 1, \
-            f'Either no mask or multiple masks found for the ID {idx}: {depth_file}'
-        assert len(feature_file) == 1, \
-            f'Either no mask or multiple masks found for the ID {idx}: {feature_file}'
-        assert len(img_file) == 1, \
-            f'Either no image or multiple images found for the ID {idx}: {img_file}'
+        #assert len(depth_file) == 1, \
+        #    f'Either no mask or multiple masks found for the ID {idx}: {depth_file}'
+        #assert len(feature_file) == 1, \
+        #    f'Either no mask or multiple masks found for the ID {idx}: {feature_file}'
+        #assert len(img_file) == 1, \
+        #    f'Either no image or multiple images found for the ID {idx}: {img_file}'
         
         feature = np.load(feature_file[0])['feature']
         depth = np.load(depth_file[0])['depth']
         img = Image.open(img_file[0]).convert('L')
 
-        assert img.size == feature.shape[:2], \
-            f'Image and feature {idx} should be the same size, but are {img.size} and {feature.shape[:2]}'
+        #assert img.size == feature.shape[:2], \
+        #    f'Image and feature {idx} should be the same size, but are {img.size} and {feature.shape[:2]}'
 
         #img = self.preprocess(img, self.scale)
         feature = self.preprocess(feature, self.scale)   ### QM: the process only transpose channel, need more data augumentation
@@ -88,10 +88,11 @@ class BasicDataset2(Dataset):
             img = np.expand_dims(img, axis=2)
         
         img_trans = img.transpose((2, 0, 1))  # CHWape(img_trans))
-
+        #feature = feature.copy()
+        
         return {
-            'feature': torch.from_numpy(feature).type(torch.FloatTensor),
-            'image': torch.from_numpy(img_trans).type(torch.FloatTensor)  # ground truth need to be considered
+            'feature': torch.from_numpy(feature.copy()).type(torch.FloatTensor),
+            'image': torch.from_numpy(img_trans.copy()).type(torch.FloatTensor)  # ground truth need to be considered
         }
 
 
@@ -112,7 +113,7 @@ class BasicDataset3(Dataset):
 
         self.ids = [splitext(file)[0] for file in listdir(imgs_dir)
                     if not file.startswith('.')]
-        logging.info(f'Creating dataset with {len(self.ids)} examples')
+        #logging.info(f'Creating dataset with {len(self.ids)} examples')
 
     def __len__(self):
         return len(self.ids)
@@ -128,15 +129,11 @@ class BasicDataset3(Dataset):
 
         if len(img_nd.shape) == 2:  # add channel to grey image
             img_nd = np.expand_dims(img_nd, axis=2)  # HWC
-        _,_,c2 = np.shape(img_nd) 
 
-
-        print(feature_nd.shape)
-        print(img_nd.shape)
+        _, _, c2 = np.shape(img_nd) 
 
         #print(img_nd.shape)
         #print(feature_nd.shape)
-        scale = 0.5
 
         if scale != 1:
             step = (1.0 /scale)  # Attention here, in the outside of images may have lost problem
@@ -153,17 +150,17 @@ class BasicDataset3(Dataset):
                         new_feature[h_num,w_num] = feature_nd[i,j,:]
                         w_num += 1
                         h_num += 1
-            print('new size',w_num,h_num)
+            #print('new size',w_num,h_num)
             w, h = newW, newH
             feature_nd = new_feature
             img_nd = new_img
 
         crop_rand_seed_w = torch.rand(1)
         crop_rand_seed_h = torch.rand(1)
-        crop_w = int(torch.floor((w - crop_size) * crop_rand_seed_w))
+        crop_w = int(torch.floor((w - crop_size) * crop_rand_seed_w))   # 640 - 480 
         crop_h = int(torch.floor((h - crop_size) * crop_rand_seed_h))
-        feature_nd = feature_nd[crop_w:crop_w+crop_size, crop_h:crop_h+crop_size,:]
-        img_nd = img_nd[crop_w:crop_w+crop_size, crop_h:crop_h+crop_size,:]
+        feature_nd = feature_nd[crop_h:crop_h+crop_size, crop_w:crop_w+crop_size, :]
+        img_nd = img_nd[crop_h:crop_h+crop_size, crop_w:crop_w+crop_size, :]
 
         # random flip
         flip_rand_seed = torch.rand(1)
@@ -177,10 +174,8 @@ class BasicDataset3(Dataset):
         img_trans = img_nd.transpose(( 2, 0, 1))    # batch
         #if img_trans.max() > 1:
         #    img_trans = img_trans / 255
-        print(feature_trans.shape)
-        print(img_trans.shape)
-        feature_trans = np.resize(feature_trans,(32 ,168, 224))  #### QM: resize so ram enough
-        img_trans = np.resize(img_trans,(1,168,224))
+        #feature_trans = np.resize(feature_trans,(32 ,168, 224))  #### QM: resize so ram enough
+        #img_trans = np.resize(img_trans,(1,168,224))
 
         return feature_trans, img_trans
 
@@ -193,14 +188,14 @@ class BasicDataset3(Dataset):
         #cv2.imread(path,0)
         #print('start get item',idx)
 
-        assert len(depth_file) == 1, \
-            f'Either no mask or multiple masks found for the ID {idx}: {depth_file}'
-        assert len(pos_file) == 1, \
-            f'Either no mask or multiple masks found for the ID {idx}: {feature_file}'
-        assert len(desc_file) == 1, \
-            f'Either no image or multiple images found for the ID {idx}: {img_file}'
-        assert len(img_file) == 1, \
-            f'Either no image or multiple images found for the ID {idx}: {img_file}'
+        #assert len(depth_file) == 1, \
+        #    f'Either no mask or multiple masks found for the ID {idx}: {depth_file}'
+        #assert len(pos_file) == 1, \
+        #    f'Either no mask or multiple masks found for the ID {idx}: {feature_file}'
+        #assert len(desc_file) == 1, \
+        #    f'Either no image or multiple images found for the ID {idx}: {img_file}'
+        #assert len(img_file) == 1, \
+        #    f'Either no image or multiple images found for the ID {idx}: {img_file}'
 
         #img = data_load.load_img(img_list[i])
         depth = np.load(depth_file[0])['depth']
@@ -210,27 +205,152 @@ class BasicDataset3(Dataset):
 
         pos = np.array(data_load.load_json(pos_file[0]))   # 3 x points_num  list, the third is confidence
         desc = np.array(data_load.load_json(desc_file[0]))  # 256 x points_num  list, 256 features
+
         pos_num = np.shape(pos)[1]
         desc_num = np.shape(desc)[1]
         assert pos_num == desc_num, 'superpoint number matching problem'
         height, width = np.shape(img)  # 480,640
-        desc_length = np.shape(desc)[0]  # 256
+        desc_length = np.shape(desc)[0]  # 256 
+
+
         #feature = np.zeros([width,height,desc_length])   # build a 640 x 480 x 256 array
-        feature = np.zeros([height,width,desc_length])    # build a 480 x 640 x 256 array   HWC
+        feature = np.zeros([height,width,desc_length+1 ])    # build a 480 x 640 x 257 array   HWC
         for j in range(pos_num):
-            x = int(pos[0][j])
-            y = int(pos[1][j])
-            feature[y,x] = desc[:,j]   # to compensate with zero
+            x = int(pos[0][j]) #640
+            y = int(pos[1][j]) #480
+            feature[y,x,1:] = desc[:,j]   # to compensate with zero
+            feature[y,x,1] = (np.array(img)[y,x]/127.5-1)
         
-        assert np.shape(img) == feature.shape[:2], \
-            f'Image and feature {idx} should be the same size, but are {img.size} and {feature.shape[:2]}'
+        #assert np.shape(img) == feature.shape[:2], \
+        #    f'Image and feature {idx} should be the same size, but are {img.size} and {feature.shape[:2]}'
 
         feature, img = self.preprocess(feature, img, self.scale, self.crop_size)   ### QM: the process only transpose channel, need more data augumentation
-    
+
+        #print(feature.shape)
+        #print(img.shape)
 
         return {
-            'feature': torch.from_numpy(feature).type(torch.FloatTensor),
-            'image': torch.from_numpy(img).type(torch.FloatTensor)  # ground truth need to be considered
+            'feature': torch.from_numpy(feature.copy()).type(torch.FloatTensor),
+            'image': torch.from_numpy(img.copy()).type(torch.FloatTensor)  # ground truth need to be considered
+        }
+class BasicDataset4(Dataset):
+    def __init__(self, image_list, depth_list,feature_list, scale, pct_3D_points, crop_size):
+        self.image_list = image_list
+        self.depth_list = depth_list
+        self.feature_list = feature_list
+        self.scale = scale
+        self.pct_3D_points = pct_3D_points
+        self.crop_size = crop_size
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+
+        self.ids = list(range(len(image_list)))
+        #logging.info(f'Creating dataset with {len(self.ids)} examples')
+
+    def __len__(self):
+        return len(self.ids)
+
+    @classmethod
+    def preprocess(cls, feature, img, scale, crop_size):
+        # feature: HWC, img in np shape: HWC. img in size WHC
+        h, w, c = np.shape(feature) 
+        #print(h,w,c) # 480, 640, 256
+  
+        feature_nd = np.array(feature)
+        img_nd = np.array(img)
+
+        if len(img_nd.shape) == 2:  # add channel to grey image
+            img_nd = np.expand_dims(img_nd, axis=2)  # HWC
+
+        _, _, c2 = np.shape(img_nd) 
+
+        #print(img_nd.shape)
+        #print(feature_nd.shape)
+
+        if scale != 1:
+            step = (1.0 /scale)  # Attention here, in the outside of images may have lost problem
+            w_num = 0
+            h_num = 0
+            newW, newH = int(scale * w), int(scale * h)
+            assert newW > 0 and newH > 0, 'Scale is too small'
+            new_img = np.zeros([newH,newW,c2])   
+            new_feature = np.zeros([newH,newW,c])
+            for i in range(h):
+                for j in range(w):
+                    if (i == int(h_num * step) and (j == int(w_num * step))):
+                        new_img[h_num,w_num] = img_nd[i,j,:]
+                        new_feature[h_num,w_num] = feature_nd[i,j,:]
+                        w_num += 1
+                        h_num += 1
+            #print('new size',w_num,h_num)
+            w, h = newW, newH
+            feature_nd = new_feature
+            img_nd = new_img
+
+        crop_rand_seed_w = torch.rand(1)
+        crop_rand_seed_h = torch.rand(1)
+        crop_w = int(torch.floor((w - crop_size) * crop_rand_seed_w))   # 640 - 480 
+        crop_h = int(torch.floor((h - crop_size) * crop_rand_seed_h))
+        feature_nd = feature_nd[crop_h:crop_h+crop_size, crop_w:crop_w+crop_size, :]
+        img_nd = img_nd[crop_h:crop_h+crop_size, crop_w:crop_w+crop_size, :]
+
+        # random flip
+        flip_rand_seed = torch.rand(1)
+        if flip_rand_seed <= 0.3:
+            feature_nd = np.flip(feature_nd,1)  # left right flip 
+            img_nd = np.flip(img_nd,1)
+
+        # HWC to CHW 
+        feature_trans = feature_nd.transpose((2, 0, 1)) # channel x 480 x 640
+        #feature_trans = (feature_trans/127.5)-1   # normalization 127.5 is for RGB, do we need this number here?
+        img_trans = img_nd.transpose(( 2, 0, 1))    # batch
+        #if img_trans.max() > 1:
+        #    img_trans = img_trans / 255
+        #feature_trans = np.resize(feature_trans,(32 ,168, 224))  #### QM: resize so ram enough
+        #img_trans = np.resize(img_trans,(1,168,224))
+
+        return feature_trans, img_trans
+
+    def __getitem__(self, i):
+        idx = self.ids[i]
+        depth_file = self.depth_list[idx]  
+        feature_file=self.feature_list[idx]   
+        image_file = self.image_list[idx]    # one image jpg
+
+        depth = np.load(depth_file,allow_pickle=True)['image_depth']
+        img = Image.open(image_file).convert('L')
+
+        #print(pos_file)
+        temp=np.load(feature_file,allow_pickle=True)
+        pos=temp['pts']
+        desc=temp['desc']
+
+
+        pos_num = np.shape(pos)[1]
+        desc_num = np.shape(desc)[1]
+        assert pos_num == desc_num, 'superpoint number matching problem'
+        height, width = np.shape(img)  # 480,640
+        desc_length = np.shape(desc)[0]  # 256 
+
+
+        #feature = np.zeros([width,height,desc_length])   # build a 640 x 480 x 256 array
+        feature = np.zeros([height,width,desc_length+1 ])    # build a 480 x 640 x 257 array   HWC
+        for j in range(pos_num):
+            x = int(pos[0][j]) #640
+            y = int(pos[1][j]) #480
+            feature[y,x,1:] = desc[:,j]   # to compensate with zero
+            feature[y,x,1] = (np.array(img)[y,x]/127.5-1)
+        
+        #assert np.shape(img) == feature.shape[:2], \
+        #    f'Image and feature {idx} should be the same size, but are {img.size} and {feature.shape[:2]}'
+
+        feature, img = self.preprocess(feature, img, self.scale, self.crop_size)   ### QM: the process only transpose channel, need more data augumentation
+
+        #print(feature.shape)
+        #print(img.shape)
+
+        return {
+            'feature': torch.from_numpy(feature.copy()).type(torch.FloatTensor),
+            'image': torch.from_numpy(img.copy()).type(torch.FloatTensor)  # ground truth need to be considered
         }
 
 class CarvanaDataset(BasicDataset2):
