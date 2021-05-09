49a48dc79bf5c4422907083337e8c0f069d2da12
Jiacheng Qiu <bobchiu001@gmail.com>
2021-05-09 16:25:52 +0200

    add r2d2 feature extraction

--- utils/dataset.py
+++ utils/dataset.py
@@ -106,6 +106,96 @@ class BasicDataset2(Dataset):
             'image': torch.from_numpy(img_trans.copy()).type(torch.FloatTensor)  # ground truth need to be considered
         }
 
+class R2D2_dataset(Dataset):
+    def __init__(self, dataset_config = {}):
+        self.augumentation_config = dataset_config.get('augumentation')
+        self.imgs_dir = self.augumentation_config['dir_img']
+        self.crop_size = self.augumentation_config['crop_size']
+        self.rescale_size = self.augumentation_config['rescale_size']
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.ids = [splitext(file)[0] for file in listdir(self.imgs_dir)
+                    if not file.startswith('.')]
+        logging.info('Creating dataset with %s examples', len(self.ids))
+
+    def __len__(self):
+        return len(self.ids)
+
+    @classmethod
+    def preprocess(cls, img, rescale_size, crop_size):
+        # include rescale, crop and flip, flip set 30% 
+        w,h = img.size
+        scale_rand_seed_w = torch.rand(1)
+        random_scale = rescale_size + (1-rescale_size)*scale_rand_seed_w
+        new_w = int(random_scale*w)
+        new_h = int(random_scale*h)
+        img = img.resize((new_w, new_h), Image.ANTIALIAS)
+        assert crop_size <= new_h and crop_size <= new_w,'crop_size is bigger than new rescale image'
+        
+        img_ = np.array(img)
+
+        crop_rand_seed_w = torch.rand(1)
+        crop_rand_seed_h = torch.rand(1)
+        crop_w = int(torch.floor((new_w - crop_size) * crop_rand_seed_w))   # 640 - 480 
+        crop_h = int(torch.floor((new_h - crop_size) * crop_rand_seed_h))
+        img_aug = img_[crop_h:crop_h+crop_size, crop_w:crop_w+crop_size]
+
+        flip_rand_seed = torch.rand(1)
+        if flip_rand_seed <= 0.3:
+            img_aug = np.flip(img_aug,1)
+        
+        return img_aug
+
+    def __getitem__(self, i):
+        idx = self.ids[i]
+        img_file = glob(self.imgs_dir + idx + '.*')     # one image jpg
+        keys = ['keypoints', 'scores', 'descriptors']
+
+        #img = data_load.load_img(img_list[i])
+        img = Image.open(img_file[0]).convert('L') # read img in greyscale
+        img_aug = self.preprocess(img, self.rescale_size, self.crop_size)
+
+        frame_tensor = frame2tensor(img_aug, 'cpu')  # attention here, frame_tensor is ground truth
+        last_data = self.superpoint({'image': frame_tensor})
+        # last_data = {k: last_data[k] for k in keys} #  ['keypoints', 'scores', 'descriptors']
+        keypoints = last_data['keypoints']
+        scores = last_data['scores']
+        desc = last_data['descriptors']
+
+        keypoints_tensor = keypoints[0]
+        # print(len(keypoints))
+        print(keypoints_tensor.size())
+        print(scores)
+
+        #keypoints_np = np.array(keypoints)
+        #scores_np = np.array(scores)
+        #desc_np = np.array(desc)
+
+        # print((keypoints_np[0].size())) #
+        #print(np.shape(scores_np)) #
+        #print(np.shape(desc_np)) #
+
+        # points_num = np.shape(keypoints)[1]
+
+        height, width = np.shape(img_aug)  # crop_size x crop_size 
+        desc_length = np.shape(desc)[0]  # 256 R2D2 is 128
+
+        feature_pad = np.zeros([height,width,desc_length])    # build a 480 x 640 x 256 array   HWC
+        for j in range(points_num):
+            x = int(keypoints[0][j]) #crop_size
+            y = int(keypoints[1][j]) #crop_size
+            feature_pad[y,x,:] = desc[:,j]   # to compensate with zero
+        
+        feature_trans = feature_pad.transpose((2,0,1)) # HWC to CHW
+        img_nd = np.expand_dims(img_aug,axis=2)
+        img_trans = img_nd.transpose((2,0,1)) # HWC to CHW
+        # after preprocess, the feature and image will be well transposed and augumented
+        # feature, img = self.preprocess(feature_pad, img, self.crop_size)   ### QM: the process only transpose channel, need more data augumentation
+
+        return {
+            'feature': torch.from_numpy(feature_trans.copy()).type(torch.FloatTensor),
+            'image': torch.from_numpy(img_trans.copy()).type(torch.FloatTensor)  # ground truth need to be considered
+        }
 
 
 # Data basic3 load pos_dir and des_dir to construct a feature 480x640x257 with other point zero
