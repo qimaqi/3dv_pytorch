c21379228f5f75778a363bb27456f540fceab812
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-11 01:45:58 +0200

    points num

--- utils/dataset.py
+++ utils/dataset.py
@@ -93,7 +93,7 @@ class BasicDataset2(Dataset):
         #print(np.shape(desc_np)) #
 
         points_num = np.shape(keypoints_np)[1]
-        # print(points_num)
+        print('points_num',points_num)
         # print(frame_tensor.size())
 
         height, width = np.shape(img_aug)  # crop_size x crop_size 
