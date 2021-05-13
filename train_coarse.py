6c7874c726410073b9e622c380648e384dfc2234
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-14 00:01:00 +0200

    batch 2 test

--- train_coarse.py
+++ train_coarse.py
@@ -223,7 +223,7 @@ def get_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-e', '--epochs', metavar='E', type=int, default=16,
                         help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=3,
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
                         help='Batch size', dest='batchsize')
     parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=5e-4,
                         help='Learning rate', dest='lr')
