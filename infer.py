3b78d0b0935677230cbfcce4b5dc1edab7664a7a
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-29 23:27:31 +0200

    val infer online2

--- infer.py
+++ infer.py
@@ -27,8 +27,8 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 # train_5k=load_annotations(os.path.join(base_image_dir,'anns/demo_5k/val.txt'))
 # train_5k_image_rgb=list(train_5k[:,4])
-infer_output_dir = '/cluster/scratch/qimaqi/data_5k/test_unet_6000_cpu_origin_max20000/'
-dir_checkpoint = '/cluster/scratch/qimaqi/checkpoints_28_5_unet_max_6000_lr1e-4/4.pth' # '/cluster/scratch/qimaqi/checkpoints_27_unet_online_max_1000_lr1e-4/8.pth' #'/cluster/scratch/qimaqi/checkpoints_28_unet_online_max_2000_lr1e-4/7.pth' 
+infer_output_dir ='/cluster/scratch/qimaqi/data_5k/test_unet_online2_cpu_origin_max20000/' #'/cluster/scratch/qimaqi/data_5k/test_unet_6000_cpu_origin_max20000/'
+dir_checkpoint = '/cluster/scratch/qimaqi/checkpoints_28_unet_online_max_2000_lr1e-4/7.pth'  # '/cluster/scratch/qimaqi/checkpoints_28_5_unet_max_6000_lr1e-4/4.pth' # '/cluster/scratch/qimaqi/checkpoints_27_unet_online_max_1000_lr1e-4/8.pth' #'/cluster/scratch/qimaqi/checkpoints_28_unet_online_max_2000_lr1e-4/7.pth' 
 base_image_dir = '/cluster/scratch/qimaqi/data_5k/data' 
 base_feature_dir  = '/cluster/scratch/qimaqi/data_5k/save_source_dir/resize_data_superpoint_1'
 # unet++
@@ -75,21 +75,23 @@ def run_infer(net,infer_loader,device):
     for batch in infer_loader:
         try:
             with torch.no_grad():
-                net.to(device=device)
-                net.eval()
-                input_features = batch['feature']
-                #name_i = batch['name']
-                #print(name_i)
-                #name_i = name_i[0]
-                #name_i = name_i.replace('/','^_^')
-                input_features = input_features.to(device=device, dtype=torch.float32)
-                pred = net(input_features).detach()
-                pred = (pred+1.)*127.5
-                pred = pred.to('cpu')
-                ouput_path = infer_output_dir + str(i)+ '.png'
-                save_image_tensor(pred,ouput_path)
-                del pred 
-                torch.cuda.empty_cache()
+                if i in (11,56,103,149,217,425,309,7,53,113,173,200,267,306,431,535,537,564,577,621,657,984):
+                # if i in (32, 317, 335, 476, 499, 36, 60, 79, 116, 118, 533, 587, 610, 650, 1003)
+                    net.to(device=device)
+                    net.eval()
+                    input_features = batch['feature']
+                    #name_i = batch['name']
+                    #print(name_i)
+                    #name_i = name_i[0]
+                    #name_i = name_i.replace('/','^_^')
+                    input_features = input_features.to(device=device, dtype=torch.float32)
+                    pred = net(input_features).detach()
+                    pred = (pred+1.)*127.5
+                    pred = pred.to('cpu')
+                    ouput_path = infer_output_dir + str(i)+ '.png'
+                    save_image_tensor(pred,ouput_path)
+                    del pred 
+                    torch.cuda.empty_cache()
                 i+=1
         except:
             pass
@@ -110,9 +112,9 @@ def run_infer(net,infer_loader,device):
             #     del pred 
             #     torch.cuda.empty_cache()
             #     i+=1
-        print('finish ',float(i)/1000)
-        if i == 100:
-           break
+        print('finish already ',i)
+        # if i == 100:
+        #    break
 
 def save_image_tensor(input_tensor, filename):
     assert (len(input_tensor.shape) == 4 and input_tensor.shape[0] == 1)
@@ -138,7 +140,7 @@ if __name__ == '__main__':
     # dataset = dataset_superpoint_5k(image_list,feature_list,img_scale, pct_3D_points, crop_size, max_points)
     # infer_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True, drop_last=True)
     # n_infer = int(len(dataset))
-    val_dataset = dataset_superpoint_5k_online(image_list,feature_list,img_scale, pct_3D_points, crop_size, max_points)
+    val_dataset = dataset_superpoint_5k_online(val_image_list,val_feature_list,img_scale, pct_3D_points, crop_size, max_points)
     infer_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True, drop_last=True)
     n_infer = int(len(val_dataset))
 
