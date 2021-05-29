d130370c4017bc4b64c101fbb6831d8bebeb1d84
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-30 00:55:10 +0200

    change val list and test list

--- infer.py
+++ infer.py
@@ -48,10 +48,9 @@ val_5k=load_annotations(os.path.join(base_image_dir,'anns/demo_5k/val.txt'))
 val_5k_image_rgb=list(val_5k[:,4])
 
 
-
 image_list=[]
 feature_list=[]
-for i in range(len(test_5k_image_rgb)):
+for i in (32,36,60,79,116,118, 317, 335, 476, 499, 533, 587, 610, 650, 1003): #range(len(test_5k_image_rgb)):
     temp_image_name=test_5k_image_rgb[i]
     temp_path=os.path.join(base_image_dir,temp_image_name)
     image_list.append(temp_path)
@@ -61,7 +60,7 @@ for i in range(len(test_5k_image_rgb)):
 
 val_image_list=[]
 val_feature_list=[]
-for i in range(len(val_5k_image_rgb)):
+for i in (7,11,53,56,103,113,149,173,200,217,267,306,309,425,431,535,537,564,577,621,657,984): #range(len(val_5k_image_rgb)):
     temp_image_name=val_5k_image_rgb[i]
     temp_path=os.path.join(base_image_dir,temp_image_name)
     val_image_list.append(temp_path)
