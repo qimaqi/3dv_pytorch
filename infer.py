6891833208a435c693589f5a07eaeecf2baf0894
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-29 12:55:40 +0200

    online cpu

--- infer.py
+++ infer.py
@@ -27,7 +27,7 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 # train_5k=load_annotations(os.path.join(base_image_dir,'anns/demo_5k/val.txt'))
 # train_5k_image_rgb=list(train_5k[:,4])
-infer_output_dir = '/cluster/scratch/qimaqi/data_5k/test_end_demo_online_cpu_256/'
+infer_output_dir = '/cluster/scratch/qimaqi/data_5k/val_end_demo_online_cpu_256/'
 dir_checkpoint = '/cluster/scratch/qimaqi/checkpoints_27_unet_online_max_1000_lr1e-4/8.pth'
 base_image_dir = '/cluster/scratch/qimaqi/data_5k/data' 
 base_feature_dir  = '/cluster/scratch/qimaqi/data_5k/save_source_dir/resize_data_superpoint_1'
