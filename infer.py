4f02540417bd7c1dd87a5d2581c9ef320642415f
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-30 13:18:00 +0200

    1000 infer

--- infer.py
+++ infer.py
@@ -28,8 +28,8 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 # train_5k=load_annotations(os.path.join(base_image_dir,'anns/demo_5k/val.txt'))
 # train_5k_image_rgb=list(train_5k[:,4])
-infer_output_dir ='/cluster/scratch/qimaqi/data_5k/val_unet++_3000_cpu_origin_max3000_3/' #'/cluster/scratch/qimaqi/data_5k/test_unet_6000_cpu_origin_max20000/'
-dir_checkpoint = '/cluster/scratch/qimaqi/checkpoints_27_5_unet++_3000/4.pth' # '/cluster/scratch/qimaqi/checkpoints_28_5_unet_max_6000_lr1e-4/4.pth' # '/cluster/scratch/qimaqi/checkpoints_27_unet_online_max_1000_lr1e-4/8.pth' #'/cluster/scratch/qimaqi/checkpoints_28_unet_online_max_2000_lr1e-4/7.pth' 
+infer_output_dir ='/cluster/scratch/qimaqi/data_5k/val_unet++_1000_cpu_origin_max1000_3/' #'/cluster/scratch/qimaqi/data_5k/test_unet_6000_cpu_origin_max20000/'
+dir_checkpoint = '/cluster/scratch/qimaqi/checkpoints_27_5_unet++_1000/4.pth' # '/cluster/scratch/qimaqi/checkpoints_28_5_unet_max_6000_lr1e-4/4.pth' # '/cluster/scratch/qimaqi/checkpoints_27_unet_online_max_1000_lr1e-4/8.pth' #'/cluster/scratch/qimaqi/checkpoints_28_unet_online_max_2000_lr1e-4/7.pth' 
 base_image_dir = '/cluster/scratch/qimaqi/data_5k/data' 
 base_feature_dir  = '/cluster/scratch/qimaqi/data_5k/save_source_dir/resize_data_superpoint_1'
 # unet++
@@ -136,7 +136,7 @@ if __name__ == '__main__':
     img_scale = 1
     crop_size = 0
     pct_3D_points = 0
-    max_points = 3000
+    max_points =1000
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  #torch.device('cpu')#
 
     # net = UNet(n_channels=256, n_classes=1)   # input should be 256, resize to 32 so ram enough
